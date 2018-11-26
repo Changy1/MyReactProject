79e29d808a80fe9a4b150c4de6457424e0f769f1
Your Name <596116693@qq.com>
2018-11-26 23:23:00 +0800

    '11.26-search-over'

--- src/pages/result/ResultList.js
+++ src/pages/result/ResultList.js
@@ -1,33 +1,78 @@
-import React, { Component } from 'react'
-import { ListForResult } from './ResultStyled'
+import React, { Component, Fragment } from 'react'
+import { ListForResult, TopForResult } from './ResultStyled'
 import GoodsItem from '@c/Goods/GoodsItem'
 import axios from 'axios'
 import connect from '@connect'
+import BetterScroll from 'better-scroll'
+import { Toast } from 'antd-mobile'
 
 class ResultList extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            items: []
+            items: [],
+            page: 1,
+            isShow: false
         }
         this.changeItems = this.changeItems.bind(this)
+        this.backTop = this.backTop.bind(this)
+        this.props.param_actions.changeSearchSort({ order: '', sort: ''})
     }
     changeItems(props) {
+        // 这里是排序的时候拿到store里面的参数
         let { search } = props.param
-        axios.get(`api/search?keyword=${props.keystr}&item=&user_groupids=p8_c4_l4_222&page=1&is_ajax=1&order=${ search.order }&sort=${ search.sort }&cat_threeids=&price_range=&filter_id=`)    
+        axios.get(`api/search?keyword=${props.keystr}&item=&user_groupids=p8_c4_l4_222&page=${this.state.page}&is_ajax=1&order=${ search.order }&sort=${ search.sort }&cat_threeids=&price_range=&filter_id=`)    
             .then(res => {
                 this.setState({
-                    items: res.data.data.list
+                    // 将更新的和之前的合并
+                    items: this.state.items.concat(res.data.data.list)
+                },() => {
+                    // 每次更新完items重新计算一次高度
+                    this.scroll.refresh()
                 })
             })
     }
-    // 第一次加载组件的时候调用这个钩子
+    // 第一次加载组件的时候调用这个钩子，只调用一次
     componentDidMount () {
         this.changeItems( this.props )
+        this.scroll = new BetterScroll('.wrapper',{
+            probeType: 2,
+            pullUpLoad: {
+                threshold: 20
+            },
+            click: true
+        })
+        // 上拉加载的方法
+        this.scroll.on('pullingUp', async () => {
+            this.loadingToast()
+            this.state.page++
+            await this.changeItems( this.props )
+            this.scroll.finishPullUp()
+            this.scroll.refresh()  
+        })
+        this.scroll.on('scrollEnd',() => {
+            if( this.scroll.y < -200 ) {
+                this.setState({ isShow: true })
+            } else {
+                this.setState({ isShow: false })
+            }
+        })
+    }
+    backTop () {
+        this.scroll.scrollTo( 0, 0, 500)
+    }
+    loadingToast() {
+        Toast.loading('正在加载...', 1);
     }
     // 在搜索页面直接进行搜索的时候需要调用这个钩子
     componentWillReceiveProps (props) {
-        this.changeItems( props )
+        // 这个方法会莫名其妙的执行，所以需要判断如果不是重新搜索或者是改变排序方法的话就不更新数据
+        if(this.props.keystr != props.keystr || this.props.param.search.order != props.param.search.order || this.props.param.search.sort != props.param.search.sort){
+            this.setState({
+                items: []
+            })
+            this.changeItems( props )
+        }
     }
     renderItem () {
         return this.state.items.map( item => (
@@ -36,9 +81,14 @@ class ResultList extends Component {
     }
     render () {
         return (
-            <ListForResult>
-                { this.renderItem() }
-            </ListForResult>
+            <Fragment>
+                <ListForResult className = 'wrapper'>
+                    <div className = 'context'>
+                        { this.renderItem() }
+                    </div>
+                </ListForResult>
+                { this.state.isShow ? <TopForResult onClick = {this.backTop} className = 'goTop'></TopForResult> : ''}
+            </Fragment>
         )
     }
 }
--- src/pages/result/ResultStyled.js
+++ src/pages/result/ResultStyled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import IconImg from '@as/images/icon.png'
+import TopImg from '@as/images/top.png'
 
 export const ResultContainer = styled.div`
     width: 100%;
@@ -9,7 +10,7 @@ export const ResultContainer = styled.div`
 export const NavForResult = styled.div`
     width: 100%;
     background: #fff;
-    border-bottom: 1px solid #ebebeb;
+    border-bottom: .013333rem solid #ebebeb;
     display: flex;
     justify-content: space-around;
     height: .8rem;
@@ -37,7 +38,22 @@ export const NavItemForResult = styled.div`
 `
 
 export const ListForResult = styled.div`
+    height: calc(100vh - 2.266667rem);
     width: 100%;
-    display: flex;
-    flex-wrap: wrap;
+    overflow: hidden;
+    .context { 
+        display: flex;
+        flex-wrap: wrap;
+    }
 ` 
+
+export const TopForResult = styled.div`
+    background-color: #fff;
+    border-radius: 50%;
+    width: 1.333333rem;
+    height: 1.333333rem;
+    background-image: ${ `url(${TopImg})` };
+    position: fixed;
+    bottom: 1.333333rem;
+    right: .266667rem;
+`
